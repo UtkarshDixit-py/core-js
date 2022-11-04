f710cb05f96d244ddc15d2ad637e7db6c9ab1242
Utkarsh <utkarshdixit303@gmail.com>
2022-11-04 18:53:47 +0530

    this keyword

--- month2/this.js
+++ month2/this.js
@@ -19,4 +19,32 @@ function person(name){
     },1000);
  }
  
-human("utkarsh2")
+human("utkarsh2");
+
+// var length = 4;
+// function callback() {
+// console.log(this.length); // What is logged?
+// }
+
+// const object = {
+//     length: 5,
+//     method() {
+//         arguments0;
+//         }
+// };
+// object.method(callback, 1, 2);
+
+
+
+
+const object = {
+    who: 'World',
+    greet() {
+    return "Hello", `${this.who}!`;
+    },
+    farewell: () => {
+    return "Goodbye", `${this.who}!`;
+    }
+};
+    console.log(object.greet()); // What is logged?
+    console.log(object.farewell()); // What is logged?
