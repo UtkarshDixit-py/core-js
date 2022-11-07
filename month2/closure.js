861d6e148963e7b99b1e0f286db0c69aa9ab7bee
Utkarsh <utkarshdixit303@gmail.com>
2022-11-07 14:51:36 +0530

    setTimeout + closures

--- month2/closure.js
+++ month2/closure.js
@@ -31,4 +31,41 @@ res()
 
 
 
+function timeOut(){
+    for(var i =1; i <=5;i++){
+        setTimeout(function(){
+            console.log(i);
+        }, i * 1000);
+    }
+    console.log("hello world");
+}
+timeOut();
 
+
+
+function timeOut3(){
+    for(var i =1; i <=5;i++){
+        function close(x){
+            setTimeout(function(){
+                console.log(x);
+            }, x * 1000);
+        }
+       close(i);
+    }
+    console.log("hello world");
+}
+timeOut3();
+
+
+
+function timeOut2(){
+    for(let i = 1; i <=5;i++){
+        setTimeout(function(){
+            console.log(i);
+        }, i * 1000);
+    }
+    console.log("hello world2");
+}
+timeOut2();
+
+ 
