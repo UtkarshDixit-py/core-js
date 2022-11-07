cf7858572652900f59c783eef78ee94e2a99d744
Utkarsh <utkarshdixit303@gmail.com>
2022-11-07 12:43:34 +0530

    closures

--- month2/closure.js
+++ month2/closure.js
@@ -3,4 +3,32 @@
     (function efg(){
         console.log(a)
     })()
-})()
+})() 
+
+
+function x(){
+    var a = 7;
+    function y(){
+        console.log(a);
+    }
+    return y;
+}
+var z = x();
+console.log(z);
+z();
+
+
+function parent(){
+    var a = 10;
+    function child(){
+        console.log(a);
+    }
+    a=100;
+    return child;
+}
+var res = parent();
+res()
+
+
+
+
