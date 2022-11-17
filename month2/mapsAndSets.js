133653587fb92d814a9964e10076ee34dad04b42
Utkarsh <utkarshdixit303@gmail.com>
2022-11-17 15:32:36 +0530

    weak map and weak sets

--- month2/mapsAndSets.js
+++ month2/mapsAndSets.js
@@ -1,11 +1,13 @@
 //maps
 let map = new Map(); //creates a map 
 
+//map.set(key,value)
 map.set("1","str1");
 map.set(1,'num1'); //numeric key
 map.set("okok", "value");
 // map.set(1,"second1") //this will replace above key value pair
 
+//map.get(key)
 console.log(map.get(1)) //num1;
 console.log(map.has(6));
 console.log(map.size);
@@ -57,4 +59,39 @@ console.log(set);
 console.log(set.size);
 console.log(set.delete(5));
 console.log(set.has(6)); 
+
+
+let john = { name : "John"};
+
+// let array = [ john ];
+
+// let myMap = new Map();
+// myMap.set(john,"random value");
+// john = null;
+
+// console.log(myMap);
+
+let myWeakMap = new WeakMap()
+
+//weakmap.set(key(must be object),value)
+myWeakMap.set(john,"random");
+
+john = null;
+
+console.log(WeakMap)
+
+
+let visited = new WeakSet();
+let p1 = {name : "utkarsh"};
+let p2 = {name : "abcde"};
+let p3 = {name : "qwerty"};
+
+visited.add(p1);
+visited.add(p2);
+
+console.log("before",visited.has(p2));
+
+p2 = null;
+
+console.log("after",visited.has(p2))
 
