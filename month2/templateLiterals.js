5f0836dff52dd48a2f6a28faed0cd1d324a331c3
Utkarsh <utkarshdixit303@gmail.com>
2022-11-17 14:49:39 +0530

    template literals

--- month2/templateLiterals.js
+++ month2/templateLiterals.js
@@ -2,21 +2,33 @@ let a = 1000;
 
 console.log(`string here ${a} string here`);
 
+function isLargeScreen(){
+    // return true;
+    return false;
+}
+
+const item ={
+    isCollapsed : false,
+}
+
 //nesting templates
 
 //without  template
 let classes = "header";
 
 classes += isLargeScreen() ? "" : item.isCollapsed ? "icon-expander" : "icon-collapser";
+console.log(classes);
 
 
 // with template literals but without nesting
 
 const className = `header ${isLargeScreen() ? "" : item.isCollapsed ? "icon-expander" : "icon-collapser" }`
+console.log(className);
 
 //with template nesting 
 var classesName = `header ${
     isLargeScreen() ? "" : `icon-${item.isCollapsed ? "expander" : "collapser"}`
 }`;
+console.log(classesName);
 
 
