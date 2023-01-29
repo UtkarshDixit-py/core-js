6942b23846a8f8056c9ae23328a558d082105e2f
UtkarshDixit-py <utkarshdixit303@gmail.com>
2023-01-29 22:33:03 +0530

    list traversal

--- month3/contact_list/index.js
+++ month3/contact_list/index.js
@@ -13,10 +13,56 @@ app.set('views',path.join(__dirname, 'views'))
 app.use('/users',userRoute);
 app.use('/profile',profileRoute);
 // app.use("*",errorRoute);
+app.use(express.urlencoded())
+
+var myList = [
+    {
+        name:"utkarsh",
+        contact:"85858585"
+
+    },
+    {
+        name:"deep work",
+        contact:"85858585"
+
+    },
+    {
+        name:"tony stark",
+        contact:"851118585"
+
+    },
+    {
+        name:"bruce wayne",
+        contact:"85858585"
+
+    },
+    {
+        name:"johny depp",
+        contact:"92858585"
+
+    },
+
+]
 
 app.get('/',function(req,res){
     console.log(req)
-    res.send('express is running')
+    // res.send('express is running')
+   return res.render('home',{
+        title : "Contact List",
+        contact_list : myList
+    })
+})
+
+app.get('/practice', function(req,res){
+   return res.render('practice',{
+        title: "playground"
+    })
+})
+
+app.post('/create-contact',function(req,res){
+    console.log("createContact req.",req.body)
+    myList.push(req.body)
+   return res.redirect('back')
 })
 
 
