51d14eff31332142cf46c5292a3452e0497b5851
UtkarshDixit-py <utkarshdixit303@gmail.com>
2023-02-07 19:20:57 +0530

    completed MERN stack contact list

--- month3/contact_list/config/mongoose.js
+++ month3/contact_list/config/mongoose.js
@@ -1,18 +1,34 @@
 
-//require mongoose
+// //require mongoose
+// const mongoose = require('mongoose');
+
+// //connect to database
+// mongoose.connect('mongodb://127.0.0.1:8000/contacts_list_db');
+
+
+// //acquire the connection
+// const db = mongoose.connection;
+
+// //error
+// db.on('error',console.error.bind(console,'error connecting db'))
+
+// //up and running then print message
+// db.once('open', function(){
+//     console.log('successfully connected to the database')
+// })
+
 const mongoose = require('mongoose');
 
-//connect to database
-mongoose.connect('mongodb://127.0.0.1:8000/contacts_list_db');
+main().catch(err => console.log("ERROR",err));
 
+async function main() {
+  await mongoose.connect('mongodb://localhost/contact_list_db');
+  
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
-//acquire the connection
-const db = mongoose.connection;
+ const db = mongoose.connection;
 
-//error
-db.on('error',console.error.bind(console,'error connecting db'))
-
-//up and running then print message
-db.once('open', function(){
+ db.once('open',function(){
     console.log('successfully connected to the database')
-})
+ })
--- month3/contact_list/index.js
+++ month3/contact_list/index.js
@@ -1,106 +1,122 @@
-const express = require('express');
-const path  = require('path');
-const db = require('./config/mongoose')
+const express = require("express");
+const path = require("path");
+const db = require("./config/mongoose");
 const port = 8000;
 
-const app = express(); 
+const app = express();
 const userRoute = require("./routes/users");
 const profileRoute = require("./routes/profile");
 const errorRoute = require("./routes/error");
+const Contact = require("./models/contact");
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use('/users',userRoute);
-app.use('/profile',profileRoute);
+app.use("/users", userRoute);
+app.use("/profile", profileRoute);
 // app.use("*",errorRoute);
-app.use(express.urlencoded())
-app.use(express.static('assets'))
+app.use(express.urlencoded());
+app.use(express.static("assets"));
 
 //middleware 1
-app.use(function(req,res,next){
-    req.myName = "Utkarsh";
-    next();
-})
+app.use(function (req, res, next) {
+  req.myName = "Utkarsh";
+  next();
+});
 
 //middleware 2
-app.use(function(req,res,next){
-    console.log("from controller 2", req.myName);
-    next()
-})
-
-
+app.use(function (req, res, next) {
+  console.log("from controller 2", req.myName);
+  next();
+});
 
 var myList = [
-    {
-        name:"utkarsh",
-        contact:"858585851"
+  {
+    name: "utkarsh",
+    contact: "858585851",
+  },
+  {
+    name: "deep work",
+    contact: "858585852",
+  },
+  {
+    name: "tony stark",
+    contact: "8511185853",
+  },
+  {
+    name: "bruce wayne",
+    contact: "858585854",
+  },
+  {
+    name: "johny depp",
+    contact: "928585856",
+  },
+];
 
-    },
-    {
-        name:"deep work",
-        contact:"858585852"
+app.get("/", function (req, res) {
+  console.log("from get / ", req.myName);
+  // res.send('express is running')
 
-    },
-    {
-        name:"tony stark",
-        contact:"8511185853"
-
-    },
-    {
-        name:"bruce wayne",
-        contact:"858585854"
-
-    },
-    {
-        name:"johny depp",
-        contact:"928585856"
-
-    },
-
-]
-
-app.get('/',function(req,res){
-    console.log("from get / ",req.myName)
-    // res.send('express is running')
-
-   return res.render('home',{
-        title : "Contact List",
-        contact_list : myList
-    })
-})
-
-app.get('/practice', function(req,res){
-   return res.render('practice',{
-        title: "playground"
-    })
-})
-
-app.post('/create-contact',function(req,res){
-    console.log("createContact req.",req.body)
-    myList.push(req.body)
-   return res.redirect('back')
-})
-
-app.get('/delete-contact/',function(req,res){
-    // /delete-contact/:phone
-    // console.log(req.params);
-
-    console.log(req.query);
-
-   let updatedList =  myList.filter((item)=>item.contact!=req.query.phone);
-   myList = updatedList;
-
-   console.log(updatedList)
-   return res.redirect('back')
-})
-
-
-
-app.listen(port,function(err){
-    if(err){
-        console.log('Error in running the server',err);
+  Contact.find({}, function (err, allContacts) {
+    if (err) {
+      console.log("error occured while retrieving data from database");
+      return;
     }
-    console.log('my express server is running on port',port)
-})
 
+    return res.render("home", {
+      title: "Contact List",
+      contact_list: allContacts,
+    });
+  });
+});
+
+app.get("/practice", function (req, res) {
+  return res.render("practice", {
+    title: "playground",
+  });
+});
+
+app.post("/create-contact", function (req, res) {
+  console.log(req.body);
+  Contact.create(
+    {
+      name: req.body.name,
+      phone: req.body.contact,
+    },
+    function (err, newContact) {
+      if (err) {
+        console.log("there is an error while adding new contact", err);
+        return;
+      }
+      console.log("****", newContact);
+
+      res.redirect("back");
+    }
+  );
+});
+
+app.get("/delete-contact/", function (req, res) {
+  // /delete-contact/:phone
+  // console.log(req.params);
+
+  let id = req.query.id
+
+  Contact.findByIdAndDelete(id,function(err){
+    if(err){
+        console.log("error while deleting the item");
+
+        return;
+    }
+
+  })
+
+
+  return res.redirect("back");
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error in running the server", err);
+  }
+  console.log("my express server is running on port", port);
+});
